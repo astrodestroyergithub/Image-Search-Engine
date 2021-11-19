d48b415444a3283bd7ad86c72f564bb73869d63c
Tamojit Roy <astrodestroyer146@gmail.com>
2021-11-19 13:52:03 +0530

    Initial commit

---
+++ course/gui.py
@@ -0,0 +1,45 @@
+import io
+import os
+import PySimpleGUI as sg
+from PIL import Image
+
+
+file_types = [("JPEG (*.jpg)", "*.jpg"),
+              ("All files (*.*)", "*.*")]
+
+def main():
+    layout = [
+        [sg.Image(key="-IMAGE-")],
+        [
+            sg.Text("Image File"),
+            sg.Input(size=(25, 1), key="-FILE-"),
+            sg.FileBrowse(file_types=file_types),
+            sg.Button("Load Image"),
+            sg.Button("Query Image", disabled=True, key="QI"),
+        ],
+    ]
+
+    window = sg.Window("Image Querying", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if event == "Load Image":
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                image.thumbnail((400, 400))
+                bio = io.BytesIO()
+                image.save(bio, format="PNG")
+                window["-IMAGE-"].update(data=bio.getvalue())
+                window["QI"].update(disabled=False)
+
+        if event == "QI":
+            window.close()
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
